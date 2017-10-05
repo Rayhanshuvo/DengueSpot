789f37dd699fa6378879febda3237bc60292c055
Your Name <you@example.com>
2017-10-05 23:02:16 +0600

    Initial commit

---
+++ app/src/main/java/database/google/com/denguespot/Admin.java
@@ -0,0 +1,40 @@
+package database.google.com.denguespot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+//In this class  getting user name and password from admin
+public class Admin extends AppCompatActivity {
+
+    EditText ET_name,ET_password;
+    String login_name,login_password;
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        ET_name=(EditText)findViewById(R.id.editText9);
+        ET_password=(EditText)findViewById(R.id.editText10);
+        btn1=(Button)findViewById(R.id.button5);
+    }
+
+    public void userLogin(View view)
+    {
+
+        login_name=ET_name.getText().toString();
+        login_password=ET_password.getText().toString();
+        String type="login";
+        admin_login admin=new admin_login(this);
+        admin.execute(type,login_name,login_password);
+
+    }
+
+   
+
+}
+
+
